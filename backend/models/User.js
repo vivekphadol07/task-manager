b7d3f823156901c702f384dbea0a2a490a31c592
vivekphadol07 <vivekphadol31@gmail.com>
2026-01-18 10:36:53 +0530

    Initial commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,40 +1,37 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
+    {
+        name: {
+            type: String,
+            required: true,
+        },
 
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
 
-    password: {
-      type: String,
-      required: true,
-    },
+        password: {
+            type: String,
+            required: true,
+        },
 
-    profileImageUrl: {
-      type: String,
-      default: null,
-    },
+        profileImageUrl: {
+            type: String,
+            default: null,
+        },
 
-    role: {
-      type: String,
-      enum: ["admin", "member"],
-      default: "member",
+        role: {
+            type: String,
+            enum: ["admin", "member"],
+            default: "member",
+        },
     },
-  },
-  {
-    timestamps: true,
-  },
+    {
+        timestamps: true,
+    }
 );
 
 module.exports = mongoose.model("User", UserSchema);
