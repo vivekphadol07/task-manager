451cf126c5e9f585d2523eb8ecd050019a5d1c35
vivekphadol07 <vivekphadol31@gmail.com>
2026-01-18 10:33:13 +0530

    Initial commit

--- backend/api/server.js
+++ backend/api/server.js
@@ -28,10 +28,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/reports", reportRoutes);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
 
 module.exports = app;
