d11c75011cdfcef0da0524f2f57a2a8f25861e8c
vivekphadol07 <vivekphadol31@gmail.com>
2026-01-20 23:14:52 +0530

    User Dashboard Fix

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -21,7 +21,7 @@ const getTasks = async (req, res) => {
                 "name email profileImageUrl"
             );
         } else {
-            tasks = await Task.find({ ...filter, assignedTo: req.user._id }).populate(
+            tasks = await Task.find({ ...filter, assignedTo: req.user.id }).populate(
                 "assignedTo",
                 "name email profileImageUrl"
             );
